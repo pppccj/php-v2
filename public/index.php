17bbfbbab99d99f66d4af6d19ffb46d290448a5e
pppccj <pchengj246403678@163.com>
2024-05-31 19:44:06 +0800

    updata

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require './../vendor/autoload.php';
+
+require '../helpers.php';
+
+use Framework\Router;
+use Framework\Session;
+
+Session::start();
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$router->route($uri);
